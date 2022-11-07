09ac821563ab20f45c758598b6604efa13052a40
jprado1 <98404937+jprado1@users.noreply.github.com>
2022-11-07 20:18:48 +0100

    Satellite beers in dataset and average rating by country/region

--- functions/NLP_utils.py
+++ functions/NLP_utils.py
@@ -4,7 +4,7 @@ import re
 import string
 from collections import Counter
 from operator import itemgetter
-
+import numpy as np
 import nltk
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -17,19 +17,37 @@ from sklearn.metrics.pairwise import linear_kernel
 tfidf = TfidfVectorizer()
 
 nltk.download('stopwords')
+def vector_space_retrieval(queries,dataframe,k=1):
+    document_dict = tokenize_dataframe(dataframe)
+    doc_vectors = tfidf.fit_transform(document_dict.values())
+    doc_ids = []
+    similarity_coefs = []
+    for query in queries.values():
+        vector_queries = tfidf.transform([query])
+        cosine_similarities = linear_kernel(vector_queries, doc_vectors).flatten()
+        related_docs_indices, cos_sim_sorted = zip(*sorted(enumerate(cosine_similarities), key=itemgetter(1), 
+                                                        reverse=True))
+        for i, cos_sim in enumerate(cos_sim_sorted):
+            if i >= k:
+                break
+            doc_ids.append(related_docs_indices[i])
+            similarity_coefs.append(cos_sim)
+    new_df = dataframe.iloc[doc_ids].copy()
+    new_df["similarity"] = np.array(similarity_coefs)
+    return new_df
+
 def tokenize(text):
     stemmer = PorterStemmer()
     text = "".join([ch for ch in text if ch not in string.punctuation])
     tokens = nltk.word_tokenize(text)
     return " ".join([stemmer.stem(word.lower()) for word in tokens])
 
-def tokenize_dataframes(dataframe, columns,key = None, filter=slice(None)):
+def tokenize_dataframe(dataframe):
     documents = {}
-    if key == None:
-        key = dataframe.index
-    for index, element in dataframe[filter].itterows():
-        document = ''.join([tokenize(str(dataframe[column])) for column in columns])
-        dataframe[key] = document
+    for index, beer in dataframe.iterrows():
+        documents[beer["beer_id"]] = (tokenize(beer["beer_name"]) + " " + tokenize(beer['brewery_name']) + " " + tokenize(str(beer["abv"])))
+    print("Documents tokenized")
+    return documents
 
 def search_vec(query, features, threshold=0.1):
     new_features = tfidf.transform([query])
@@ -43,6 +61,28 @@ def search_vec(query, features, threshold=0.1):
         doc_ids.append(related_docs_indices[i])
     return doc_ids
 
-def test():
-    print("blub!")
+def color(df,similarity,threshold=0.6):
+    good = f"background-color:#C6F8D3" 
+    bad = f"background-color:#F8C6C6" 
+    color_df = pd.DataFrame('',index=df.index,columns=df.columns)
+    BA_good_fits = similarity[similarity["BA_similarity"] > threshold ].index
+    RB_good_fits = similarity[similarity["RB_similarity"] > threshold ].index
+    BA_bad_fits = similarity[similarity["BA_similarity"] <= threshold ].index
+    RB_bad_fits = similarity[similarity["RB_similarity"] <= threshold ].index
+    color_df.loc[BA_good_fits,'BA_beer_name'] = good
+    color_df.loc[RB_good_fits,'RB_beer_name'] = good
+    color_df.loc[BA_bad_fits,'BA_beer_name'] = bad
+    color_df.loc[RB_bad_fits,'RB_beer_name'] = bad
+    return color_df
 
+def querify(query_df):
+    sat_queries = {}
+    for index, biere in query_df.iterrows():
+        sat_queries[index] = (tokenize(biere['nom']) +" "  + tokenize(biere['brasseur']) + " " + tokenize(str(biere['alcool'])))
+    return sat_queries
+
+def display_results_df(base_df,results_df,reference_column,results):
+    new_df = pd.DataFrame()
+    new_df[reference_column] = base_df[reference_column]
+    new_df[results] = results_df[results].copy()
+    return new_df
--- functions/read_data.py
+++ functions/read_data.py
@@ -4,25 +4,24 @@ import pandas as pd
 import tarfile
 import gzip
 import re
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+import json
 MAX_CSV_SIZE = 1000000
 CHUNK_SIZE = 200
 def fetch_satellite_data():
     return 0
 
-def fetch_csv(dataset_path, csv=None):
-    try:
-        with tarfile.open(dataset_path) as tar:
-            csv_list = []
-            for filename in tar.getnames():
-                if ".csv" in filename:
-                    csv_list.append(filename)
-            dataframe_list = []
-            for csv in csv_list:    
-                with tar.extractfile(csv) as file:
-                    dataframe_list.append(pd.read_csv(file))
-        return dataframe_list
-    except: 
-        print("Euh, no file was found in this path")
+def fetch_csv(dataset_path, name):
+    with tarfile.open(dataset_path) as tar:
+        dataframe = pd.DataFrame()
+        for filename in tar.getnames():
+            if name in filename:   
+                with tar.extractfile(filename) as file:
+                    dataframe = pd.read_csv(file)
+    return dataframe
+
 
 def fetch_reviews(dataset_path, max_csv_size = MAX_CSV_SIZE,early_stop = 0):
     tarfile_name = re.search("[ \w-]+?(?=\.)",dataset_path)[0]
@@ -51,14 +50,15 @@ def fetch_reviews(dataset_path, max_csv_size = MAX_CSV_SIZE,early_stop = 0):
                             if early_stop and csv_count == early_stop : 
                                 break
                     else:
-                        (_,value) = line.split(":", 1)
-                        review.append(value.rstrip())
+                        (key,value) = line.split(":", 1)
+                        #BeerAdvocate has one column called "review" that is useless and makes everything harder
+                        if key != "review":
+                            review.append(value.rstrip())
     
     df = pd.DataFrame.from_dict(review_dict, orient="index")
     df.to_csv(f"../data/{tarfile_name.replace('.tar','')}_{filename.replace('.txt.gz','')}_part_{csv_count}.csv")
     del review_dict #Just to not kill my memory please disregard :)
     print(f"Dumping data to csv number {csv_count}...")
-    print(i)
     print("Success!")
     if early_stop :
         return 1
@@ -66,3 +66,14 @@ def fetch_reviews(dataset_path, max_csv_size = MAX_CSV_SIZE,early_stop = 0):
         return df
 #    except: 
 #        print("Euh, no file was found in this path")
+
+def fetch_satellite_df():
+
+    url = "https://satellite.bar/bar/"
+    soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+    beer = re.findall('var djangoBoissons = (.*?);\s*$', soup.prettify(), re.M)
+    beerJson = beer[0]
+    satellite_dict = json.loads(beerJson)
+    satellite_df = pd.DataFrame(satellite_dict)
+
+    return satellite_df
